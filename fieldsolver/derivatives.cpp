050bb8704dafdaf749c9f5377bd8fcd04b8e3a7d
JonasSuni <jonas.suni@helsinki.fi>
2024-09-26 16:57:43 +0300

    Test normalising B first

--- fieldsolver/derivatives.cpp
+++ fieldsolver/derivatives.cpp
@@ -809,8 +809,10 @@ void calculateScaledDeltas(
       Bperp = std::sqrt(Bperp);
    }
 
+   Real myBmag = std::sqrt(std::pow(myB[0], 2) + std::pow(myB[1], 2) + std::pow(myB[1],2))
+
    // Now, rotation matrix to get parallel and perpendicular pressure
-   Eigen::Quaterniond q {Eigen::Quaterniond::FromTwoVectors(Eigen::Vector3d{myB[0], myB[1], myB[2]}, Eigen::Vector3d{0, 0, 1})};
+   Eigen::Quaterniond q {Eigen::Quaterniond::FromTwoVectors(Eigen::Vector3d{myB[0] / myBmag, myB[1] / myBmag, myB[2] / myBmag}, Eigen::Vector3d{0, 0, 1})};
    q.normalize();
    Eigen::Matrix3d rot = q.toRotationMatrix();
    // Eigen::Matrix3d rot = Eigen::Quaterniond::FromTwoVectors(Eigen::Vector3d{myB[0], myB[1], myB[2]}, Eigen::Vector3d{0, 0, 1}).toRotationMatrix();
