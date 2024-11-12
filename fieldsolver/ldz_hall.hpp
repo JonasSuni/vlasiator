bc6fc574198bc38ac68fcda7ce584ba48cb24630
JonasSuni <jonas.suni@helsinki.fi>
2024-11-12 14:51:27 +0200

    Revert "Refactor ldz_hall.cpp"
    This reverts commit a216bef51280db1736cbfd84706260a7da7c5802.

--- fieldsolver/ldz_hall.hpp
+++ fieldsolver/ldz_hall.hpp
@@ -38,20 +38,4 @@ void calculateHallTermSimple(
    const bool communicateMomentsDerivatives
 );
 
-// Add helper function declarations
-enum class HallComponent {X, Y, Z};
-
-template<HallComponent C>
-void calculateEdgeHallTermComponents(
-   FsGrid< std::array<Real, fsgrids::bfield::N_BFIELD>, FS_STENCIL_WIDTH>& perBGrid,
-   FsGrid< std::array<Real, fsgrids::ehall::N_EHALL>, FS_STENCIL_WIDTH>& EHallGrid,
-   FsGrid< std::array<Real, fsgrids::moments::N_MOMENTS>, FS_STENCIL_WIDTH>& momentsGrid,
-   FsGrid< std::array<Real, fsgrids::dperb::N_DPERB>, FS_STENCIL_WIDTH>& dPerBGrid,
-   FsGrid< std::array<Real, fsgrids::dmoments::N_DMOMENTS>, FS_STENCIL_WIDTH>& dMomentsGrid,
-   FsGrid< std::array<Real, fsgrids::bgbfield::N_BGB>, FS_STENCIL_WIDTH>& BgBGrid,
-   FsGrid< fsgrids::technical, FS_STENCIL_WIDTH>& technicalGrid,
-   const std::array<Real, Rec::N_REC_COEFFICIENTS>& perturbedCoefficients,
-   cint i, cint j, cint k
-);
-
 #endif
