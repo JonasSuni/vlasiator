fd0e00e45a7443c62d2c770eb02e3433f14e76bc
Jonas Suni <jonas.suni@helsinki.fi>
2025-03-30 19:02:19 +0300

    Do not skip first refinement

--- vlasiator.cpp
+++ vlasiator.cpp
@@ -1097,7 +1097,7 @@ int simulate(int argn,char* args[]) {
             logFile << "(AMR): Adapting refinement!"  << endl << writeVerbose;
             refineNow = false;
 
-            if (!adaptRefinement(mpiGrid, technicalGrid, sysBoundaryContainer, *project, true)) {
+            if (!adaptRefinement(mpiGrid, technicalGrid, sysBoundaryContainer, *project, false)) {
                // OOM, rebalance and try again
                logFile << "(LB) AMR rebalancing with heavier refinement weights." << endl << write;
                addTimedBarrier("barrier-amr-rebalance-heavier");
