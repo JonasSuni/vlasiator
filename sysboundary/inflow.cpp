58ec2f51c5195ee7bcecf24e00832fcffa7e61ac
Jonas Suni <jonas.suni@helsinki.fi>
2025-07-06 19:24:22 +0300

    Fix inflow

--- sysboundary/inflow.cpp
+++ sysboundary/inflow.cpp
@@ -253,6 +253,11 @@ namespace SBC {
       EGradPeGrid.get(i, j, k)->at(fsgrids::egradpe::EXGRADPE + component) = 0.0;
    }
 
+   void Inflow::fieldSolverBoundaryCondDJdtElectricField(
+      FsGrid<std::array<Real, fsgrids::edjdt::N_EDJDT>, FS_STENCIL_WIDTH>& EDJdtGrid, cint i, cint j, cint k, cuint component) {
+      EDJdtGrid.get(i, j, k)->at(fsgrids::edjdt::EXDJDTE + component) = 0.0;
+   }
+
    void Inflow::fieldSolverBoundaryCondDerivatives(
       FsGrid<std::array<Real, fsgrids::dperb::N_DPERB>, FS_STENCIL_WIDTH>& dPerBGrid,
       FsGrid<std::array<Real, fsgrids::dmoments::N_DMOMENTS>, FS_STENCIL_WIDTH>& dMomentsGrid, cint i, cint j, cint k,
