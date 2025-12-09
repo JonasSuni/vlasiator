eb754606e6d94dd42c281aaef386c32e92be3426
Jonas Suni <jonas.suni@helsinki.fi>
2025-12-09 17:08:27 +0200

    Test

--- sysboundary/sysboundary.cpp
+++ sysboundary/sysboundary.cpp
@@ -812,6 +812,6 @@ void SysBoundary::updateSysBoundariesAfterLoadBalance(dccrg::Dccrg<SpatialCell, 
    }
 }
 
-bool SysBoundary::checkSysBoundaryExistence(cuint sysboundarytype) {
-   return this->sysBoundaryExistence[sysboundarytype];
+bool SysBoundary::checkSysBoundaryExistence(cuint sbctype) {
+   return this->sysBoundaryExistence[sbctype];
 }
