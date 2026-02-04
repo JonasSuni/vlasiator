37d1b6e9d7abacbc7852c50bebc93f32d73e54c7
JonasSuni <jonas.suni@helsinki.fi>
2026-02-04 11:51:06 +0200

    More transfer parts

--- grid.cpp
+++ grid.cpp
@@ -566,6 +566,7 @@ void balanceLoad(dccrg::Dccrg<SpatialCell,dccrg::Cartesian_Geometry>& mpiGrid, S
    // ...and finally we reduce this across all tasks of course.
    MPI_Allreduce(&num_part_transfers_local, &num_part_transfers, 1, MPI_UINT64_T, MPI_MAX, MPI_COMM_WORLD);
 
+   num_part_transfers *= 2;
    for (uint64_t transfer_part=0; transfer_part<num_part_transfers; transfer_part++) {
       //Set transfers on/off for the incoming cells in this transfer set and prepare for receive
       for (unsigned int i=0;i<incoming_cells_list.size();i++){
