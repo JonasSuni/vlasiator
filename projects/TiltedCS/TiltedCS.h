a4368cb5106b467554fc8887e7bbe34de9814a0c
JonasSuni <jonas.suni@helsinki.fi>
2026-06-22 15:42:17 +0300

    Initial commit for tilted current sheet project

---
+++ projects/TiltedCS/TiltedCS.h
@@ -0,0 +1,105 @@
+/*
+ * This file is part of Vlasiator.
+ * Copyright 2010-2016 Finnish Meteorological Institute
+ *
+ * For details of usage, see the COPYING file and read the "Rules of the Road"
+ * at http://www.physics.helsinki.fi/vlasiator/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+//#pragma once
+
+#ifndef TILTEDCS_H
+#define TILTEDCS_H
+
+#include "../../definitions.h"
+#include "../projectTriAxisSearch.h"
+
+namespace projects {
+
+   struct TiltedCSSpeciesParameters {
+      Real V0u[3];
+      Real DENSITYu;
+      Real TEMPERATUREu;
+      Real V0utangential;
+      Real Vucosphi;
+
+      Real V0d[3];
+      Real DENSITYd;
+      Real TEMPERATUREd;
+      Real V0dtangential;
+      Real Vdcosphi;
+
+      int Vyusign;
+      int Vydsign;
+      int Vzusign;
+      int Vzdsign;
+
+      Real maxwCutoff;
+   };
+
+   class TiltedCS: public TriAxisSearch {
+   public:
+      TiltedCS();
+      virtual ~TiltedCS();
+
+      virtual bool initialize(void) override;
+      static void addParameters(void);
+      virtual void getParameters(void) override;
+
+      virtual void setProjectBField(fsgrids::perbspan perb,
+                                    fsgrids::bgbspan bgb,
+                                    fsgrids::technicalspan technical, FieldSolverGrid& fsgrid) override;
+
+      virtual Realf fillPhaseSpace(spatial_cell::SpatialCell* cell, const uint popID,
+                                   const uint nRequested) const override;
+      virtual Realf probePhaseSpace(spatial_cell::SpatialCell* cell, const uint popID, Real vx_in, Real vy_in,
+                                    Real vz_in) const override;
+      virtual std::vector<std::array<Real, 3>> getV0(creal x, creal y, creal z, const uint popID) const override;
+      virtual void calcCellParameters(spatial_cell::SpatialCell* cell, creal& t) override;
+      bool
+      refineSpatialCells(dccrg::Dccrg<spatial_cell::SpatialCell, dccrg::Cartesian_Geometry>& mpiGrid) const override;
+      // Interpolate between up- and downstream quantities
+      // based on position
+      Real interpolate(Real u, Real d, Real x) const;
+
+      // Upstream bulk values
+      Real B0u[3];
+      Real B0utangential;
+      // Downstream bulk values
+      Real B0d[3];
+      Real B0dtangential;
+
+      // Flow direction definitions
+      Real Bucosphi;
+      Real Bdcosphi;
+      int Byusign;
+      int Bydsign;
+      int Bzusign;
+      int Bzdsign;
+
+      Real Shockwidth;
+      Real AMR_L1width;
+      Real AMR_L2width;
+      Real AMR_L3width;
+      Real AMR_L4width;
+
+      std::vector<TiltedCSSpeciesParameters> speciesParams;
+
+   } ; //class TiltedCS
+} // namespace projects
+
+#endif
--- projects/project.cpp
+++ projects/project.cpp
@@ -53,6 +53,7 @@
 #include "../backgroundfield/constantfield.hpp"
 #include "Shocktest/Shocktest.h"
 #include "LossCone/LossCone.h"
+#include "TiltedCS/TiltedCS.h"
 #include "../sysboundary/sysboundarycondition.h"
 
 #ifdef DEBUG_VLASIATOR
@@ -96,6 +97,7 @@ namespace projects {
       projects::verificationLarmor::addParameters();
       projects::Shocktest::addParameters();
       projects::LossCone::addParameters();
+      projects::TiltedCS::addParameters();
       RP::add("Project_common.seed", "Seed for the RNG", 42);
 
    }
@@ -714,6 +716,9 @@ Project* createProject() {
    }
    if(Parameters::projectName == "LossCone") {
       rvalue = new projects::LossCone;
+   }
+   if(Parameters::projectName == "TiltedCS") {
+      rvalue = new projects::TiltedCS;
    }
 
 
