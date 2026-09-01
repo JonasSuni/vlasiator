668c9dc94e7a143d1904b706044ddfadbe473493
JonasSuni <JonasSuni@users.noreply.github.com>
2026-09-01 11:52:58 +0000

    deploy: 506b9a6fb3a4188664ed6e039415f72f95368343

---
+++ navtreedata.js
@@ -0,0 +1,117 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Vlasiator", "index.html", [
+    [ "Vlasiator &ndash; 10 letters you can count on", "index.html", null ],
+    [ "Vlasiator User's Manual", "d0/db1/page1.html", [
+      [ "Installation", "d0/db1/page1.html#sec-install", null ],
+      [ "Program options", "d0/db1/page1.html#sec-options", null ],
+      [ "Writing a project", "d0/db1/page1.html#sec-project", null ]
+    ] ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", "namespacemembers_vars" ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", "namespacemembers_eval" ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", "globals_vars" ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d0/d93/cpu__face__estimates_8hpp.html#a1055fa9804604bd272f1cc1fcdf61d18a1f0991e6c76b7386a6c554438580db7c",
+"d0/dbf/struct_parameters.html#ab5b859fafb940acad4f90286b6fb4dd0",
+"d1/d08/namespacebackground__dots.html#a6d942f677a31782c8cd976c82692ce51",
+"d1/d3f/classspatial__cell_1_1_spatial_cell.html#a18a753b9eee066e9b1f20cddd66eeada",
+"d1/d3f/classspatial__cell_1_1_spatial_cell.html#ab3b5bf63a685b711ee0fbfb093fd971b",
+"d1/d5d/namespacebackground.html#a7884c3d47c551c0935bf09bad0402e68",
+"d1/d9f/acceleration-vlasiator_2map__test_8cpp.html#a5ecd67c56a9621d507886b719ddaaa11",
+"d1/de8/class_d_r_o_1_1_variable_p_tensor_thermal_off_diagonal.html#a3b7db01778382478d2b01b8c1847877a",
+"d3/d0d/namespacevectorpotentialdipole__verify.html#a463528d00e7067e5538e03d9aa1703e2",
+"d3/d98/map__test__3d_8cpp.html#ac3aae0bd9c77a2556ed0e8a7754414b9",
+"d4/d3a/fluxfunction_8cpp.html#acf20bf5d310404b3cc6f0f193df9575d",
+"d4/d8d/struct_s_b_c_1_1_spherical_tri_grid.html#a8f4c87ae03fe37a64440e74e1c047d19",
+"d4/dfe/ldz__volume_8cpp.html#aac267aa1275b8d429263cedfa906f86b",
+"d5/d71/class_s_b_c_1_1_outflow.html#a275d67610002ef686ba7d515d44da38b",
+"d5/db6/classprojects_1_1_shock.html#adcc7897b64ca62785067277c8e6c2cb6",
+"d6/d9e/class_d_r_o_1_1_variable_pressure_solver.html#aeffd3a116a0fb33af0ae324a4fb8b4d0",
+"d7/dd0/namespacespatial__cell_1_1_transfer.html#ae6b10ac54b6fa92b53dfc0eba6ce5621",
+"d8/da5/namespacevectorpotentialdipole__streamlines2.html#a83c0ef60199d9f030a290e730c6126cf",
+"d8/ddb/class_s_b_c_1_1_sys_boundary_condition.html#a919e9d13a92c4d823bb0609fcd82ef7a",
+"d9/d69/classprojects_1_1_distributions.html#a262a974972908c33134cae738409c921",
+"d9/da0/classprojects_1_1_loss_cone.html#a8af49aaac9beee01753e024ed093d5e9",
+"d9/df1/namespaceprojects.html#ad8acbdd937116027025fa1835286ccf2",
+"da/d6b/class_s_b_c_1_1_ionosphere.html#a74bb854669a6edfe669c25c37b8b7552",
+"da/dea/class_d_r_o_1_1_data_reduction_operator_fs_grid.html#a42f058d9ae5fcbfa23c8857472073a44",
+"db/d4a/classprojects_1_1_i_p_shock.html#a67545ad61eadcb449475e7900db5fe92",
+"db/d79/arch__device__cuda_8h.html#adfcec253448c8568eaa8f70e712284e8",
+"dc/d2e/gpu__pitch__angle__diffusion_8cpp.html#a46edf1b9cadc904e8a0ba57e683f4a14",
+"dc/d39/classvmesh_1_1_velocity_mesh.html#acc3dc9b3dbba9a720232786e4c24d995",
+"dc/d9e/class_physical_constants_s_i.html",
+"dc/dd5/classprojects_1_1_firehose.html#a11ebc9559178489284eef3740397404d",
+"dd/d47/struct_particle.html",
+"dd/d93/relativistic__math_8h_source.html",
+"de/d23/classprojects_1_1_larmor.html#abdd5cd8b91e949f1e8a052be5b13dcb4",
+"de/d6a/class_s_b_c_1_1_inflow.html#ac16a71672ae3d024ef55196f5a0d1799",
+"de/db7/velocity__mesh__parameters_8cpp.html",
+"df/d47/structprojects_1_1_dispersion_species_parameters.html",
+"df/dfc/version_8h.html#a82851d3226e0d140e69672280f94ea2c",
+"namespacemembers_d.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
+var LISTOFALLMEMBERS = 'List of all members';
